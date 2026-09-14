6e67ff644ec3e72b8e5207d605cbe3de89edf481
miiadraz <mdrazenovic6@gmail.com>
2026-09-14 13:32:26 +0200

    feat: add WatchlistItem type

---
+++ src/types/shows.ts
@@ -0,0 +1,30 @@
+export interface Show {
+  id: number;
+  name: string;
+  summary: string | null;
+  status: string;
+  premiered: string | null;
+  rating: {
+    average: number | null;
+  };
+  genres: string[];
+  image: {
+    medium: string;
+    original: string;
+  } | null;
+  network: {
+    name: string;
+  } | null;
+}
+
+export interface SearchResult {
+  score: number;
+  show: Show;
+}
+
+export interface WatchlistItem {
+  id: number;
+  name: string;
+  image: string | null;
+  status: "watching" | "planned" | "completed";
+}
